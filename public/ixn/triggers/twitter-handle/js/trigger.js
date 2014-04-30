4053a2af2b6e4eef007c08eab00582fecbcb9474
Gary Borton <gborton@exacttarget.com>
2014-04-30 14:57:36 -0400

    Empty out the et and ui payload.

--- public/ixn/triggers/twitter-handle/js/trigger.js
+++ public/ixn/triggers/twitter-handle/js/trigger.js
@@ -31,13 +31,21 @@ define( function( require ) {
                 twitterFollowers: $twitterFollowers.val()
             };
 
-            uiPayload = {
+            /*uiPayload = {
                 options: data,
                 description: 'This is a hello world trigger configuration instance.'
+            };*/
+            uiPayload = {
+
             };
 
-            etPayload = {
+            /*etPayload = {
                 filter: "<FilterDefinition Source='SubscriberAttribute'><ConditionSet Operator='AND' ConditionSetName='Grouping'><Condition ID='268133f8-fbcf-e311-9ae6-ac162db18844' isParam='false' Operator='GreaterThanOrEqual' operatorTemplate='undefined' operatorEditable='1' valueEditable='1' conditionValid='1'><Value><![CDATA[" + data.twitterFollowers + "]]></Value></Condition></ConditionSet></FilterDefinition>"
+            };
+            */
+
+            etPayload = {
+
             };
 
             connection.trigger( 'save', uiPayload, etPayload );
