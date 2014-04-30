ff086449f83e804994453fd9f89dbbb557fc0e87
Gary Borton <gborton@exacttarget.com>
2014-04-29 23:53:40 -0400

    Add updates to the createTweet route.

--- app.js
+++ app.js
@@ -159,29 +159,27 @@ app.post('/fireEvent/:type', function( req, res ) {
     }
 });
 
-app.post('/createTweet', function (req, res) {
+app.post('/createTweet', function (req, response) {
+    if (!req.body.tweet) {
+        response.send(400, 'The tweet param is required.');
+    }else {
+        var radian6Host = 'https://api.radian6.com';
+        var path = '/socialcloud/v1/twitter/status?async=true';
+        var requestOptions = {
+            url: radian6Host + path,
+            headers: {
+                'auth_appkey': 'radian6-integration',
+                'auth_token': '0a0c0201030887702d7344d5eeda3bff5a1a1e86844c9ac2c418db92b996dabaad221de16c739914322db675ec53c530c326b08b884e',
+                'X-R6-SMMAccountId': '42802',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+                status: req.body.tweet
+            }
+        };
 
-    var radian6Host = 'https://api.radian6.com';
-    var path = '/socialcloud/v1/twitter/status?async=true';
-    var requestOptions = {
-        url: radian6Host + path,
-        headers: {
-            'auth_appkey': 'radian6-integration',
-            'auth_token': '0a0c0201030887702d7344d5eeda3bff5a1a1e86844c9ac2c418db92b996dabaad221de16c739914322db675ec53c530c326b08b884e',
-            'X-R6-SMMAccountId': '42802',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        method: 'POST',
-        body: {
-            status: 'This+is+a+test'
-        }
-    };
-
-    req.pipe(request(requestOptions, function (error, innerResponse, body) {
-        console.log('error -', error);
-        console.log('response -', innerResponse);
-        console.log('body -', body);
-    })).pipe(res);
+        req.pipe(request.post(requestOptions)).pipe(response);
+    }
 });
 
 app.get('/clearList', function( req, res ) {
