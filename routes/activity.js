352432b14334e1f4ed87f53af518f2409702c71f
Iulian Mihai <imihai@exacttarget.com>
2014-04-30 13:59:35 -0400

    modify th activity

--- routes/activity.js
+++ routes/activity.js
@@ -30,26 +30,27 @@ exports.save = function( req, res ) {
  */
 exports.execute = function( req, response ) {
 	console.log(req);
-	if (!req.body.tweet) {
-		response.send(400, 'The tweet param is required.');
-	}else {
-		var radian6Host = 'https://api.radian6.com';
-		var path = '/socialcloud/v1/twitter/status?async=true';
-		var requestOptions = {
-			url: radian6Host + path,
-			headers: {
-				'auth_appkey': 'radian6-integration',
-				'auth_token': '0a0c0201030887702d7344d5eeda3bff5a1a1e86844c9ac2c418db92b996dabaad221de16c739914322db675ec53c530c326b08b884e',
-				'X-R6-SMMAccountId': '42802',
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			form: {
-				status: req.body.tweet
-			}
-		};
-
-		req.pipe(request.post(requestOptions)).pipe(response);
-	}
+//	if (!req.body.tweet) {
+//		response.send(400, 'The tweet param is required.');
+//	}else {
+//		var radian6Host = 'https://api.radian6.com';
+//		var path = '/socialcloud/v1/twitter/status?async=true';
+//		var requestOptions = {
+//			url: radian6Host + path,
+//			headers: {
+//				'auth_appkey': 'radian6-integration',
+//				'auth_token': '0a0c0201030887702d7344d5eeda3bff5a1a1e86844c9ac2c418db92b996dabaad221de16c739914322db675ec53c530c326b08b884e',
+//				'X-R6-SMMAccountId': '42802',
+//				'Content-Type': 'application/x-www-form-urlencoded'
+//			},
+//			form: {
+//				status: req.body.tweet
+//			}
+//		};
+//
+//		req.pipe(request.post(requestOptions)).pipe(response);
+	//}
+	response.send(200);
 };
 
 /*
