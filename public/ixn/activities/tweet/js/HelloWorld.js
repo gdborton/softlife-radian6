31dabc33062561c60059dd8965a27ff1207cba4e
Iulian Mihai <imihai@exacttarget.com>
2014-04-30 16:33:45 -0400

    added payload

--- public/ixn/activities/tweet/js/HelloWorld.js
+++ public/ixn/activities/tweet/js/HelloWorld.js
@@ -43,7 +43,9 @@ define( function( require ) {
 
 		//TODO: Shouldn't this come from the data?
 	    payload.flowDisplayName = "Send Tweet";
-	    payload.tweetContent = $('#txtTweet').val();
+	    payload.tweetContent = "Congratulation you won a personal JetPack"
+
+		    $('#txtTweet').val(payload.tweetContent);
 
 
 	    connection.trigger('getPayload', payload);
@@ -54,6 +56,8 @@ define( function( require ) {
 	// consists of the Event Data and passes it to the
 	// "config.js.save.uri" as a POST
     connection.on('populateFields', function(payload) {
+
+
     });
 
 	// Trigger this method when updating a step. This allows JB to
