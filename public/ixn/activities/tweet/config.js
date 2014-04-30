9012619045107cde6e897940087ceff23c0a4339
Gary Borton <gborton@exacttarget.com>
2014-04-30 17:08:59 -0400

    Change inArguments to use amp script.

--- public/ixn/activities/tweet/config.js
+++ public/ixn/activities/tweet/config.js
@@ -16,7 +16,7 @@ define([], function(){
        "execute": {
             "uri": "https://softlife.herokuapp.com/ixn/activities/tweet/execute/",
 			"inArguments": [
-                { "Name": "twitterHandle", "DefaultValue": "" }
+                "%%twitterHandle%%"
             ],
 			"outArguments": [],
             "verb": "POST",
