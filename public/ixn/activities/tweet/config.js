7df22d722ab19b52d2452d9f4f74549686aeb9c8
Iulian Mihai <imihai@exacttarget.com>
2014-04-30 12:14:53 -0400

    added tweet activity

---
+++ public/ixn/activities/tweet/config.js
@@ -0,0 +1,57 @@
+define([], function(){      
+    return {
+        "icon": "images/jb-icon.jpg",
+        "iconSmall": "images/jb-icon.jpg", 
+        "key": "key-softlife-tweet-activity",
+        "partnerApiObjectTypeId": "IXN.CustomActivity.REST",
+        "lang": {
+            "en-US": {        
+                "name": "Softlife Tweeter Activity",
+                "description": "This activity sends tweets to users."
+            }
+        },
+        "category": "messaging",
+        "version": "1.0",
+        "apiVersion": "1.0",
+       "execute": {
+            "uri": "https://softlife.herokuapp.com/ixn/activities/tweet/execute/",
+			"inArguments": [],
+			"outArguments": [],
+            "verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+		},
+        "save": {
+            "uri": "https://softlife.herokuapp.com/ixn/activities/tweet/save/",
+			"verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+        },
+        "publish": {
+            "uri": "https://softlife.herokuapp.com/ixn/activities/tweet/publish/",
+            "verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+        },
+        "validate": {
+            "uri": "https://softlife.herokuapp.com/ixn/activities/tweet/validate/",
+            "verb": "POST",
+			"body": "",
+            "format": "json",
+            "useJwt": false,
+            "timeout": 3000
+        },
+
+        "edit": {
+            "uri": "https://softlife.herokuapp.com/ixn/activities/tweet/",
+            "height": 400,
+            "width": 500
+        }
+};
+});
