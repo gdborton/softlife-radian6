651da6413f971a72cf43d4b9ed11b82b160e6e5a
Iulian Mihai <imihai@exacttarget.com>
2014-04-30 22:15:16 -0400

    added mongohq call

--- public/ixn/activities/tweet/js/tweet.js
+++ public/ixn/activities/tweet/js/tweet.js
@@ -76,7 +76,7 @@ define( function( require ) {
 				console.log(JSON.stringify(data));
 			    var row="";
 			    for(var i=0;i<data.length;i++) {
-				    row += "<tr><td>" +data[i].tweetContent + "<td></tr>";
+				    row += "<tr><td><b>Tweet:</b></td><td>" +data[i].tweetContent + "<td></tr>";
 			    }
 
 			    var table = "<table>"+ row +"</table>";
