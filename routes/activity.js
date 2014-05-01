4f14c690780d74bab8ad95c21e50d6c267edbd78
Gary Borton <gborton@exacttarget.com>
2014-05-01 02:51:45 -0400

    Start tweeting with a randomly selected tweet.

--- routes/activity.js
+++ routes/activity.js
@@ -49,9 +49,27 @@ exports.execute = function( req, response ) {
 			if (err) {
 				return console.error(err)
 			}
-			docs.forEach(function (doc) {
-				console.log('found document: ', doc)
-			});
+
+            var selectedIndex = Math.round(Math.random() * (docs.length - 0));
+            var tweet = docs[selectedIndex].tweetContent;
+
+            var radian6Host = 'https://api.radian6.com';
+            var path = '/socialcloud/v1/twitter/status?async=true';
+            var requestOptions = {
+                url: radian6Host + path,
+                headers: {
+                    'auth_appkey': 'radian6-integration',
+                    'auth_token': '0a0c0201030887702d7344d5eeda3bff5a1a1e86844c9ac2c418db92b996dabaad221de16c739914322db675ec53c530c326b08b884e',
+                    'X-R6-SMMAccountId': '42802',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                form: {
+                    status: tweet
+                }
+            };
+
+            req.pipe(request.post(requestOptions)).pipe(response);
+
 		});
 	});
 
@@ -61,22 +79,7 @@ exports.execute = function( req, response ) {
 	if (!req.body.tweet) {
 		response.send(400, 'The tweet param is required.');
 	}else {
-		var radian6Host = 'https://api.radian6.com';
-		var path = '/socialcloud/v1/twitter/status?async=true';
-		var requestOptions = {
-			url: radian6Host + path,
-			headers: {
-				'auth_appkey': 'radian6-integration',
-				'auth_token': '0a0c0201030887702d7344d5eeda3bff5a1a1e86844c9ac2c418db92b996dabaad221de16c739914322db675ec53c530c326b08b884e',
-				'X-R6-SMMAccountId': '42802',
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			form: {
-				status: req.body.tweet
-			}
-		};
 
-		req.pipe(request.post(requestOptions)).pipe(response);
 	}
 };
 
